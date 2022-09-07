48bf220675b34b4167631a9ba910a869963063ef
Aiden <aiden-99@hotmail.co.uk>
2022-09-07 17:41:45 +0100

    Add and display template messages

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,29 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
+const formatDate = (date) => {
+  const toString = date.toString();
+  const newDate = toString.split(" ").slice(0, 6).join(" ");
+
+  return `Sent at ${newDate}`;
+};
+
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: formatDate(new Date()),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: formatDate(new Date()),
+  },
+];
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Mini messageboard", messages: messages });
 });
 
 module.exports = router;
