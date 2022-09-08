3d9b762b61ddc4fa175859813e993f344e8494bf
Aiden <aiden-99@hotmail.co.uk>
2022-09-08 15:49:53 +0100

    Add date formatting function and ability to post new messages

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,15 @@
 var express = require("express");
+const { response } = require("../app");
 var router = express.Router();
 
 const formatDate = (date) => {
   const toString = date.toString();
-  const newDate = toString.split(" ").slice(0, 6).join(" ");
+  const newDate = `${toString
+    .split(" ")
+    .slice(1, 4)
+    .join(" ")} at ${toString.slice(16, 21)}`;
 
-  return `Sent at ${newDate}`;
+  return newDate;
 };
 
 const messages = [
@@ -24,6 +28,16 @@ const messages = [
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.render("index", { title: "Mini messageboard", messages: messages });
+});
+
+router.post("/new", function (req, res, next) {
+  messages.push({
+    text: req.body.message,
+    user: req.body.user,
+    added: formatDate(new Date()),
+  });
+
+  res.redirect("/");
 });
 
 module.exports = router;
